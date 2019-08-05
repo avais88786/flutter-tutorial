70c532a865b26153afdcd55f65f78d9ebeab1e61
Avaisuddin Mohammad <avaisuddin.mohammad@outlook.com>
2019-08-05 23:04:10 +0100

    part 2 - click

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class RandomWords extends StatefulWidget {
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = Set<WordPair>();
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +52,11 @@ class RandomWordsState extends State<RandomWords> {
         title: Text('Startup Name Generator'),
       ),
       body: _buildSuggestions(),
-      bottomNavigationBar: Text("A Footer", style: _biggerFont, textAlign: TextAlign.center,),
+      bottomNavigationBar: Text(
+        "A Footer",
+        style: _biggerFont,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 
@@ -70,11 +75,25 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: ()  {
+        setState(()  {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
