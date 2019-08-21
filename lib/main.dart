b917b7d9306c1af3dd3cf3b930ced05dcec41071
Avais Mohammad <avais.mohammad@opengi.co.uk>
2019-08-21 16:20:43 +0100

    Error Handling - wip

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,31 @@ import 'package:tutorial/themewidget.dart';
 
 import 'favouriteslist.dart';
 import 'layouttutorial.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+//final SentryClient _sentry = SentryClient(dsn: "App DSN goes Here");
+
+Future<Null> main() async {
+// This captures errors reported by the Flutter framework.
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    if (isInDebugMode) {
+      // In development mode simply print to console.
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      // In production mode report to the application zone to report to
+      // Sentry.
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
+  runZoned<Future<Null>>(() async {
+    runApp(MyApp());
+  }, onError: (error, stackTrace) async {
+    await reportError(error, stackTrace);
+  });
+}
+
+//void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -41,5 +64,38 @@ class MyApp extends StatelessWidget {
       },
       //home: RandomWords(),
     );
+  }
+}
+
+bool get isInDebugMode {
+  bool inDebugMode = false;
+
+// Assert expressions are only evaluated during development. They are ignored
+  // in production. Therefore, this code only sets `inDebugMode` to true
+  // in a development environment.
+  assert(inDebugMode = true);
+
+  return inDebugMode;
+}
+
+Future<Null> reportError(dynamic error, dynamic stackTrace) async {
+  if (isInDebugMode) {
+    print(stackTrace);
+    print('In dev mode. Not sending report to Sentry.io.');
+    return;
+  }
+
+  //print('Reporting to Sentry.io...');
+
+  // final SentryResponse response = await _sentry.captureException(
+  //   exception: error,
+  //   stackTrace: stackTrace,
+  // );
+
+  if (5 == 5) {
+    //response.isSuccessful
+    print('Reported Success!');
+  } else {
+    print('Failed to report to Sentry.io:');
   }
 }
